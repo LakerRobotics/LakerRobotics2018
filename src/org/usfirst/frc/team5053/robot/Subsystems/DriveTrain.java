7afa0fb53457b645cf3852a0890d0e8843e37a32
IMAGESOFT\cross <cross@imagesoftinc.com>
2018-02-08 15:44:34 -0500

    Added swing turn ability to DriveTrain class.

--- src/org/usfirst/frc/team5053/robot/Subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team5053/robot/Subsystems/DriveTrain.java
@@ -4,19 +4,21 @@ import java.util.HashMap;
 
 import org.usfirst.frc.team5053.robot.Subsystems.Utilities.AnglePIDWrapper;
 import org.usfirst.frc.team5053.robot.Subsystems.Utilities.DistancePIDWrapper;
+import org.usfirst.frc.team5053.robot.Subsystems.Utilities.SwingPIDWrapper;
 
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 /**
  * Drivetrain subsystem that extends the FRC RobotDrive class.
  * @author Colin Ross
  *
  */
 
-public class DriveTrain extends RobotDrive implements Subsystem
+public class DriveTrain extends DifferentialDrive implements Subsystem
 {
 	/**
 	 * Hello There! : I'm the base constructor.
@@ -27,9 +29,11 @@ public class DriveTrain extends RobotDrive implements Subsystem
 	
 	private PIDController m_DistancePID;
 	private PIDController m_AnglePID;
+	private PIDController m_SwingPID;
 	
 	private DistancePIDWrapper m_DistancePIDWrapper;
 	private AnglePIDWrapper m_AnglePIDWrapper;
+	private SwingPIDWrapper m_SwingPIDWrapper;
 	
 	
 	private Encoder m_LeftEncoder;
@@ -39,6 +43,8 @@ public class DriveTrain extends RobotDrive implements Subsystem
 	
 	private double m_speed = 0.0;
 	private double m_turn = 0.0;
+	private double m_swingTurnValue = 0.0;
+	private boolean m_swingTurnLeft = false;
 	
 	public DriveTrain(SpeedController leftMotor, SpeedController rightMotor) 
 	{
@@ -92,6 +98,7 @@ public class DriveTrain extends RobotDrive implements Subsystem
 		
 		m_DistancePIDWrapper = new DistancePIDWrapper(this);
 		m_AnglePIDWrapper = new AnglePIDWrapper(this);
+		m_SwingPIDWrapper = new SwingPIDWrapper(this);
 		
 		
 		//this.setExpiration(0.1);
@@ -102,6 +109,9 @@ public class DriveTrain extends RobotDrive implements Subsystem
 		
 		m_AnglePID = new PIDController(0.1, 0.0, 0.0, m_AnglePIDWrapper, m_AnglePIDWrapper);
 		m_AnglePID.setAbsoluteTolerance(2.5);
+		
+		m_SwingPID = new PIDController(0.1, 0.0, 0.0, m_SwingPIDWrapper, m_SwingPIDWrapper);
+		m_SwingPID.setAbsoluteTolerance(2.5);
 		
 		System.out.println("Constructor finished");
 	}
@@ -146,6 +156,7 @@ public class DriveTrain extends RobotDrive implements Subsystem
 		m_turn = angle;
 		this.ArcadeDrive(speed, angle);
 	}
+	
 	public void SetSpeed(double speed)
 	{
 		this.ArcadeDrive(speed, m_turn);
@@ -199,6 +210,9 @@ public class DriveTrain extends RobotDrive implements Subsystem
 		if (m_AnglePID.isEnabled()) {
 			m_AnglePID.disable();
 		}
+		if (m_SwingPID.isEnabled()) {
+			m_SwingPID.disable();
+		}
 	}
 	public boolean DistanceOnTarget()
 	{
@@ -219,6 +233,45 @@ public class DriveTrain extends RobotDrive implements Subsystem
 		m_AnglePID.setSetpoint(angle);
 		
 	}
+	public boolean StartSwingTurn() {
+		try {
+			if (!m_SwingPID.isEnabled()) {
+				m_SwingPID.enable();
+			}
+			return true;
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return false;
+		
+	}
+	public boolean SetSwingParameters(double angle, boolean isLeft) {
+		try {
+			m_SwingPID.setSetpoint(angle);
+			m_swingTurnLeft = isLeft;
+			
+			return true;
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+		return false;
+	}
+	public void SwingTurn(double turnSpeed) {
+		m_swingTurnValue = turnSpeed;
+		
+		if (m_swingTurnLeft) {
+			this.tankDrive(0, turnSpeed);
+		} else {
+			this.tankDrive(turnSpeed, 0);
+		}
+	}
+	public boolean SwingAngleOnTarget()
+	{
+		if(Math.abs(GetSwingPIDSetpoint() - GetAngle()) < 2.5)
+		{
+			return true;
+		} else return false;
+	}
 	double GetDistancePIDSetpoint() 
 	{
 		return m_DistancePID.getSetpoint();
@@ -226,6 +279,10 @@ public class DriveTrain extends RobotDrive implements Subsystem
 	double GetAnglePIDSetpoint()
 	{
 		return m_AnglePID.getSetpoint();
+	}
+	double GetSwingPIDSetpoint()
+	{
+		return m_SwingPID.getSetpoint();
 	}
 	public HashMap<String, Double> GetDashboardData() {
 		return null;
