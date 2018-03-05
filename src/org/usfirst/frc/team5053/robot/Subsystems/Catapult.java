57dcbe2ea218dd069efb87f99c5ae0ab6a2bcafa
Richard G. Topolewski <topolewr@cooley.edu>
2018-03-04 22:19:58 -0500

    Added a more accurate time limited catapult throw uses a seperate thread
    After writing this did some research and this post explains this a
    little bit https://www.chiefdelphi.com/forums/showthread.php?t=127327
    indicates they have success with this approach.
    Michael indicate for short shot 5-6 cycles at 0.02 so 0.1 to 0.12 for a
    short shot so maybe 0.2 second shot.
    
    This thread from TORC indicates accuracy standard deviation is from
    0.0037 or 0.0007 seconds but that was labview.
    
    Also this post from Cheesy Poof says usually 0.005 but occasionally 0.2
    https://www.chiefdelphi.com/forums/showpost.php?p=1477882&postcount=17
    
    So in end I think depends on CPU utilization we will have to test to see
    how repeatable it will be.

--- src/org/usfirst/frc/team5053/robot/Subsystems/Catapult.java
+++ src/org/usfirst/frc/team5053/robot/Subsystems/Catapult.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team5053.robot.Subsystems;
 
 
+import java.time.Instant;
+
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -20,6 +22,41 @@ public class Catapult implements Subsystem {
 	{
 		m_LeftSolenoid.set(Value.kForward);
 		m_RightSolenoid.set(Value.kForward);
+	}
+	/**
+	 *  pass in fractions of a seconds to throw with the piston before reversing 
+	 *  
+	 */
+	public void LauchTimeLimited(double secondsTillReverse ) {
+		long milliSecondsTillReverse = (long) (secondsTillReverse*1000);
+		int nanoSecondTillReverse = (int) (secondsTillReverse*1000*1000000 - milliSecondsTillReverse*1000000);
+
+		//Define the sepearte thread 
+        Thread t = new Thread(() -> {
+        	try {
+        		Instant start = Instant.now();
+        		m_LeftSolenoid.set(Value.kForward);
+        		m_RightSolenoid.set(Value.kForward);
+        		
+        		Thread.sleep(milliSecondsTillReverse, nanoSecondTillReverse);
+        		
+        		m_LeftSolenoid.set(Value.kReverse);
+        		m_RightSolenoid.set(Value.kReverse);
+        		Instant end = Instant.now();
+        		
+        		double howLongActualFired = (end.getNano()-start.getNano())/1000000000;
+        		
+        		System.out.println("Catapult.LaunchTimeLimited thread: Wanted to fire for "+secondsTillReverse+" seconds; Catapult actually Fired for "+howLongActualFired+ " seconds.  Error was " +(howLongActualFired -secondsTillReverse)+ " seconds.");
+        		
+        	} catch (InterruptedException e) {
+        		// TODO Auto-generated catch block
+        		e.printStackTrace();
+        	}
+        });
+        
+        // start the thread that will run the above code totally indipendently of the normal robot thread
+        t.start();
+
 	}
 	public void Arm() 
 	{
