6cf4a4a616337e103505fc04ca03721e2f00d336
IMAGESOFT\cross <cross@imagesoftinc.com>
2018-03-12 13:24:34 -0400

    Final code version from Waterford.

--- src/org/usfirst/frc/team5053/robot/Robot.java
+++ src/org/usfirst/frc/team5053/robot/Robot.java
@@ -1288,21 +1288,25 @@ public void disabledInit(){
     	} 
     	/*else if (Math.abs(m_RobotInterface.GetOperatorJoystick().getRawAxis(2)) > .05){
     		m_Intake.AdjustableSpeed(m_RobotInterface.GetOperatorJoystick().getRawAxis(2));
-    	} 
+    	} */
     	// if joystick left-right more then 5% or twist more then 5% then variable joystick control of intake (note any button above the buttons take precedent and we never get here)
-    	else if (  Math.abs(m_RobotInterface.GetOperatorJoystick().getRawAxis(2)) > .15)
+    	else if (  Math.abs(m_RobotInterface.GetOperatorJoystick().getRawAxis(2)) > .15 && m_RobotInterface.GetOperatorButton(8))
     	{
     		// if twist is less then 5% then just straight intake
     		if( Math.abs(m_RobotInterface.GetOperatorJoystick().getRawAxis(3)) < .15) 
     		{
-    			m_Intake.AdjustableSpeedWithTwist(m_RobotInterface.GetOperatorJoystick().getRawAxis(2), 0);
+    			
+    			m_Intake.AdjustableSpeedWithTwist(0.80, m_RobotInterface.GetOperatorJoystick().getRawAxis(2));
     		// else twist power is more then 5% so apply variable twist power
     		}
     		else 
     		{
-    			m_Intake.AdjustableSpeedWithTwist(m_RobotInterface.GetOperatorJoystick().getRawAxis(2), m_RobotInterface.GetOperatorJoystick().getRawAxis(3));
+    			double speed = m_RobotInterface.GetOperatorJoystick().getRawAxis(3);
+    			speed = (speed + 1)/2;
+    			
+    			m_Intake.AdjustableSpeedWithTwist(speed, m_RobotInterface.GetOperatorJoystick().getRawAxis(2));
     		}
-    	}*/
+    	}
     	else if (!m_RobotInterface.GetOperatorButton(8))
     	{
     		m_Intake.StopIntake();
